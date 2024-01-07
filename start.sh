a555ddde2b38ff5658fc7067e4c5f5b6d7481120
AndersenLi108 <129189624+AndersenLi108@users.noreply.github.com>
2024-01-07 21:12:24 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,9 +19,9 @@ while [ "$shard_id" -lt $1 ]; do
         if [ $(( $shard_id * $2 + $node_id )) -ge $6 ] && [ $(( $shard_id * $2 + $node_id )) -lt $(($6 + $7)) ]; then
           echo "start node $node_id in shard $shard_id..."
           python3 run_socket_node.py --sid 'sidA' --id $node_id --shard_id $shard_id --shard_num $1 --N $2 --f $3 --B $4 --R $5 --tx_num $8 &
-          rm "./TXs_file/TXs$(($shard_id * $2 + $node_id))"
+          #rm "./TXs_file/TXs$(($shard_id * $2 + $node_id))"
           rm "./log/consensus-node-$(($shard_id * $2 + $node_id)).log"
-          cp ./TXs "./TXs_file/TXs$(($shard_id * $2 + $node_id))"
+          #cp ./TXs "./TXs_file/TXs$(($shard_id * $2 + $node_id))"
         else
           echo "do nothing"
         fi
